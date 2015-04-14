520b9ce223d4c80cc8793d5bec117f2c71a9d5b7
Ubuntu User <ubuntu@ubuntu-armhf.(none)>
2015-04-14 05:44:12 +0000

    Working Test.py

--- Python_Code/Ultrasonic.py
+++ Python_Code/Ultrasonic.py
@@ -1,56 +1,38 @@
 """
-Class for handling the Ultrasonic Sensor
+Function for handling the Ultrasonic Sensor
 """
 import Adafruit_BBIO.ADC as ADC
 import logging
 import time
 
-class Ultrasonic:
-    def __init__(self):
+def getDist(self):
 
-        #setup ADC
-        ADC.setup()
-        logging.basicConfig(filename = 'testlog.log', filemode = 'w', level=logging.DEBUG)
+	while self.count < 10:
 
-        self.value = 0
-        self.voltage = 0
-        self.distance = 0
-        self.count = 0
-        self.readings = []
-        self.summed = 0
-        self.average = 0
+	    #Take a reading
+	    self.value = ADC.read("P9_40")
+	    self.voltage = self.value * 1.5
 
-    def get_distance(self):
+	    #Do conversion math here
+	    self.distance = self.voltage / 0.00008901 * 1.175
 
-        while self.count < 10:
+	    if self.distance > (self.readings(count - 1) + 500):
+		self.distance = self.readings(count - 1)
+	    elif self.distance <= (self.readings(count - 1) - 500):
+		self.distance = self.readings(count - 1)
 
-            #Take a reading
-            self.value = ADC.read("P9_40")
-            self.voltage = self.value * 1.5
+	    self.readings.append(self.distance)
 
-            #Do conversion math here
-            self.distance = self.voltage / 0.00008901 * 1.175
+	    self.summed = sum(self.readings)
 
-            if self.distance > (self.readings(count - 1) + 500):
-                self.distance = self.readings(count - 1)
-            elif self.distance <= (self.readings(count - 1) - 500):
-                self.distance = self.readings(count - 1)
+	    self.count += 1
 
-            self.readings.append(self.distance)
+	self.average = self.summed/10.0
 
-            self.summed = sum(self.readings)
+	self.count = 0
+	del self.readings[:]
 
-            self.count += 1
-
-        self.average = self.summed/10.0
-
-
-        self.count = 0
-        del self.readings[:]
-
-        time.sleep(0.025)
-        print "Distance is %d" %self.average
-        logging.debug('Distance is %f' %self.average)
-        return self.average
-
- 
+	time.sleep(0.025)
+	print "Distance is %d" %self.average
+	logging.debug('Distance is %f' %self.average)
+	return self.average
